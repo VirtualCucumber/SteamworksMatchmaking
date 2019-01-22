4030ff5a013f68182146cf3a1294152278db4ec2
Ian <virtualcucumber@hotmail.com>
2019-01-22 15:42:33 -0600

    Initial commit

---
+++ Assets/Scripts/Steamworks/SteamClient.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Facepunch.Steamworks;
+
+public class SteamClient : MonoBehaviour
+{
+    public uint AppId;
+
+    private Client client;
+
+    private void Awake()
+    {
+        // Create the client
+        client = new Client(AppId);
+    }
+
+    void Start()
+    {
+        // keep us around until the game closes
+        GameObject.DontDestroyOnLoad(gameObject);
+
+        if (AppId == 0)
+            throw new System.Exception("You need to set the AppId to your game");
+
+        //
+        // Configure us for this unity platform
+        //
+        Config.ForUnity(Application.platform.ToString());
+
+
+
+        if (!client.IsValid)
+        {
+            client = null;
+            Debug.LogWarning("Couldn't initialize Steam");
+            return;
+        }
+
+        Debug.Log("Steam Initialized: " + client.Username + " / " + client.SteamId);
+    }
+
+    void Update()
+    {
+        if (client == null)
+            return;
+
+        try
+        {
+            UnityEngine.Profiling.Profiler.BeginSample("Steam Update");
+            client.Update();
+        }
+        finally
+        {
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (client != null)
+        {
+            client.Dispose();
+            client = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Steamworks/SteamNetworking.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facepunch.Steamworks;
+
+public class SteamNetworking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Client.Instance.Lobby.OnLobbyCreated += OnLobbyCreatedCallback;
+        Client.Instance.LobbyList.OnLobbiesUpdated += OnLobbiesUpdatedCallback;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Trying to create a lobby...");
+            Client.Instance.Lobby.Create(Lobby.Type.Public, 10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("Trying to get list of lobbies...");
+            Client.Instance.LobbyList.Refresh();
+        }
+
+    }
+
+    void OnLobbyCreatedCallback(bool success)
+    {
+        if (success)
+            Debug.Log(Client.Instance.Lobby.Owner + " created a lobby");
+        else
+            Debug.Log("Lobby creation failed");
+    }
+
+    void OnLobbiesUpdatedCallback()
+    {
+        Debug.Log("Active lobbies = " + Client.Instance.LobbyList.Lobbies.Count);
+    }
+}
